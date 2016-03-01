a19491804a980ff93ebd6401319298c9461da4e1
Izel Nakri <contact@izelnakri.com>
2016-03-01 23:47:34 +0100

    small add

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,7 +1,7 @@
 // no need for a fancy flux store for this example, not a very fancy store ;)
 var store = {};
 
-$.getJSON('/data.json').then(function (data) {
+$.getJSON('data.json').then(function (data) {
     store = data;
     riot.mount('*');
 });
